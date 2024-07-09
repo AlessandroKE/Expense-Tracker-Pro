a1403d398d8153356feba66fe970fbdb874eed49
SandroKE <koomealessandro@gmail.com>
2024-07-09 04:20:09 -0600

    Connecting MongoDB and Creation of Models

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const app = express();
 const errorHandlers = require('./handlers/errorhandlers');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
 
 // Other routes and middleware
 // app.use(...)
 app.use(express.json());
+
+mongoose.connect(process.env.mongo_connection,{}).then(()=>{
+    console.log('Connection is established');
+}).catch(() => {
+    console.log('Mongo connection failed')
+})
 
 // Error handling middleware should be defined last
 app.use(errorHandlers);
