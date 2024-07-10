f0d8a9fdccba8ff3504aa8ca2a3549c6278ebba3
SandroKE <koomealessandro@gmail.com>
2024-07-10 08:28:28 -0600

    added user model

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const errorHandlers = require('./handlers/errorhandlers');
+const errorHandlers = require('./handlers/errorHandlers');
 const mongoose = require('mongoose');
 
 require('dotenv').config();
