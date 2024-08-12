f43fa4a4426eb09269ebaaded0e719d584685f89
SandroKE <koomealessandro@gmail.com>
2024-08-12 13:23:57 -0600

    updated transactions controller

--- modules/transactions/controllers/getTransactions.js
+++ modules/transactions/controllers/getTransactions.js
@@ -1,9 +1,22 @@
-const getTransactions  = /* async */ (req, res) =>{
+const mongoose = require('mongoose');
 
 
+
+const getTransactions  = async (req, res) =>{
+
+    const transactionModel = mongoose.model('transactions');
+
+    const user_id = req.user._id;
+
+    const transaction = await transactionModel.find({
+        //Query strings and spread operators
+        user_id,
+        ...req.query
+
+    })
     res.status(200).json({
         status: "success",
-        message: "Hello from transactions"
+        Data: transaction
     })
 }
 
