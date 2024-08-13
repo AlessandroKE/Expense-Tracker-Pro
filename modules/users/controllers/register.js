df8ed78a5d113da2e478c44407ea34828a0b3be7
SandroKE <koomealessandro@gmail.com>
2024-08-13 08:21:37 -0600

    sending welcome mails to new users

--- modules/users/controllers/register.js
+++ modules/users/controllers/register.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
+const nodemailer = require('nodemailer')
+
 const jwtManager = require('../../../manager/jwtManager');
 const register = async (req, res) => {
   const usersModel = mongoose.model("users");
@@ -31,6 +33,52 @@ const register = async (req, res) => {
     });
 
     const accessToken =  jwtManager(createdUser);
+
+  /*   var transport = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: "imranabdi2704@gmail.com",
+        pass: "exghoikktolvolgw"
+      }
+    });
+ */
+
+    var transport = nodemailer.createTransport({
+      host: "sandbox.smtp.mailtrap.io",
+      port: 2525,
+      auth: {
+        user: "795825fe7c7634",
+        pass: "f54ff3719a6e47"
+      }
+    });
+
+    await transport.sendMail({
+      to: createdUser.email,
+      from: "support@expense.tracker.com",
+      subject: "Welcome to Expense Tracker Pro!",
+      html: `
+        <div style="font-family: Arial, sans-serif; color: #333; margin: 0; padding: 20px; background-color: #f4f4f4;">
+          <div style="background-color: #ffffff; border-radius: 8px; box-shadow: 0 0 10px rgba(0, 0, 0, 0.1); padding: 20px; max-width: 600px; margin: auto;">
+            <h1 style="color: #007bff; font-size: 24px;">Hi ${createdUser.full_name},</h1>
+            <h2 style="color: #333; font-size: 20px;">Welcome to Expense Tracker Pro!</h2>
+            <p style="font-size: 16px; color: #333;">
+              We're excited to have you on board. With Expense Tracker Pro, you'll be able to easily manage your expenses, track your income, and get a clearer picture of your financial health.
+            </p>
+            <p style="font-size: 16px; color: #333;">
+              If you have any questions or need assistance, feel free to reach out to us at <a href="mailto:support@expense.tracker.com" style="color: #007bff;">support@expense.tracker.com</a>.
+            </p>
+            <p style="font-size: 16px; color: #333;">
+              Happy tracking!
+            </p>
+            <p style="font-size: 16px; color: #333;">
+              Best regards,<br>
+              The Expense Tracker Pro Team
+            </p>
+          </div>
+        </div>
+  `
+    });
+    
 
     res.status(201).json({ 
       status: "User registered successfully!", 
